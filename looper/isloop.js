0bd615ee49ffdf68118c7d7f8d3f63b11a3a79b4
Treagan Birbal <treaganbirbal@pursuit.org>
2020-05-28 17:04:44 -0400

    implmentation of findLoop using set

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,4 +1,6 @@
 'use strict';
+const LinkedList = require('./linkedlist');
+
 
 //Complete this algo
 const isLoop = (linkedlist) => {
@@ -18,6 +20,27 @@ const isLoop = (linkedlist) => {
     return false;
 };
 
+const findLoop = (list) => {
+    let visited = new Set()
+    if(isLoop(list)){
+        let current = list.head
+        while(!visited.has(current)){
+            visited.add(current) 
+            current = current.next
+        }
+        return current
+    }
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const test1 = new LinkedList();
+test1.addToHead(0)
+numbers.forEach(val => test1.addToTail(val));
+test1.getNthNode(6).next = test1.getNthNode(3);
+
+console.log('ours', findLoop(test1))
+// console.log('theirs', test1.getNthNode(3))
 
 /*
 EXTRA CREDIT:
@@ -27,4 +50,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = {isLoop, findLoop}
--- looper/isloop.spec.js
+++ looper/isloop.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 const {expect} = require('chai');
 const LinkedList = require('./linkedlist');
-const isLoop = require('./isloop');
+const {isLoop, findLoop} = require('./isloop');
 
 const numbers = [1,2,3,4,5,6,7,8,9,10];
 const letters = ['a','b','c','d','e','f','g','h','i'];
@@ -36,3 +36,10 @@ describe('isLoop function', () => {
 		expect(isLoop(test3)).to.equal(false);
 	});
 });
+
+
+describe('findLoop function', () => {
+	it('returns node where loop is detected', () => {
+		expect(findLoop(test1)).to.equal(test1.getNthNode(3))
+	})
+})
