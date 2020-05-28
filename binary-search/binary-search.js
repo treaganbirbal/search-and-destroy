f3d609b8f108023ecb2e88c3eced89e074cfab69
Treagan Birbal <treaganbirbal@pursuit.org>
2020-05-28 15:18:18 -0400

    test implmentation of binarySearch

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,9 +2,23 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let front = 0
+	let back = array.length - 1
+	while(front <= back){
+	  let mid = Math.floor(front+back / 2)
+		if(target === array[mid]){
+			return true;
+		} else if(target < array[mid]){
+			back = mid - 1
+		} else if(target > array[mid]){
+			front = mid + 1
+		}
+	}
+	return false
 };
 
+let data = [1, 3, 5, 7, 9, 11]
+console.log(binarySearch(data))
 /*
 	EXTRA CREDIT:
 
